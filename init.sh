c14e2acbcc3fae8250305f5fb4e90d742260f1f2
Ariel Malka <ariel@chronotext.org>
2015-05-28 16:38:05 +0300

    BOOST 1.58. NO NEED TO PATCH ANYMORE, THANKS TO CRYSTAX (https://github.com/crystax/android-vendor-boost-1-58-0)

--- init.sh
+++ init.sh
@@ -1,20 +1,14 @@
 #!/bin/sh
 
-#
-# BASED ON https://github.com/MysticTreeGames/Boost-for-Android/blob/master/build-android.sh
-#
-
-. `dirname $0`/build-common.sh
-
 BOOST_VER1=1
-BOOST_VER2=55
+BOOST_VER2=58
 BOOST_VER3=0
 
 BOOST_VER="${BOOST_VER1}_${BOOST_VER2}_${BOOST_VER3}"
-BOOST_TAR="boost_${BOOST_VER}.tar.bz2"
-BOOST_SRC="http://downloads.sourceforge.net/project/boost/boost/${BOOST_VER1}.${BOOST_VER2}.${BOOST_VER3}/${BOOST_TAR}"
+BOOST_TAR="master.zip"
+BOOST_SRC="https://github.com/arielm/android-vendor-boost-${BOOST_VER1}-${BOOST_VER2}-${BOOST_VER3}/archive/${BOOST_TAR}"
 
-BOOST_DIR="boost_${BOOST_VER}"
+BOOST_DIR="boost"
 rm -rf $BOOST_DIR
 
 # -----------
@@ -38,48 +32,16 @@ fi
 echo "Unpacking..."
 
 if [ $(which pv) ]; then
-  pv $BOOST_TAR | tar xjf - -C $PROGDIR
+  pv $BOOST_TAR | tar xjf -
 else
   tar xjf $BOOST_TAR
 fi
 
-if [ ! -d $BOOST_DIR ]; then
+BOOST_TMP_DIR="android-vendor-boost-${BOOST_VER1}-${BOOST_VER2}-${BOOST_VER3}-master"
+
+if [ ! -d $BOOST_TMP_DIR ]; then
   echo "Unpacking failed!"
   exit 1
 fi
 
-# --------
-# PATCHING
-# --------
-
-PATCH_BOOST_DIR=$PROGDIR/patches/boost-${BOOST_VER}
-
-echo "Patching..."
-
-for dir in $PATCH_BOOST_DIR; do
-  if [ ! -d "$dir" ]; then
-    echo "Could not find directory '$dir' while looking for patches"
-    exit 1
-  fi
-
-  PATCHES=`(cd $dir && ls *.patch | sort) 2> /dev/null`
-
-  if [ -z "$PATCHES" ]; then
-    echo "No patches found in directory '$dir'"
-    exit 1
-  fi
-
-  for PATCH in $PATCHES; do
-    PATCH=`echo $PATCH | sed -e s%^\./%%g`
-    SRC_DIR=$PROGDIR/$BOOST_DIR
-    PATCHDIR=`dirname $PATCH`
-    PATCHNAME=`basename $PATCH`
-    echo "Applying $PATCHNAME to $SRC_DIR/$PATCHDIR"
-    cd $SRC_DIR && patch -p1 < $dir/$PATCH && cd $PROGDIR
-    if [ $? != 0 ]; then
-      echo "Patching failed!"
-      echo "Problematic patch: $dir/$PATCHNAME"
-      exit 1
-    fi
-  done
-done
+mv ${BOOST_TMP_DIR} ${BOOST_DIR}
