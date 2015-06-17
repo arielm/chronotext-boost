5adb0c1d1a8a9c73234778eb711e5694ab66c403
Ariel Malka <ariel@chronotext.org>
2015-06-17 14:52:34 +0300

    MINOR TWEAKS...

--- setup.sh
+++ setup.sh
@@ -8,35 +8,31 @@ BOOST_DIR="android-vendor-boost-${BOOST_VER1}-${BOOST_VER2}-${BOOST_VER3}-master
 BOOST_ZIP="master.zip"
 BOOST_SRC="https://github.com/arielm/android-vendor-boost-${BOOST_VER1}-${BOOST_VER2}-${BOOST_VER3}/archive/${BOOST_ZIP}"
 
-rm -rf dist
-
-# -----------
-# DOWNLOADING
-# -----------
+# ---
 
 if [ ! -f $BOOST_ZIP ]; then
   echo "DOWNLOADING $BOOST_SRC"
   curl -L -O $BOOST_SRC
+
+  if [ $? != 0 ] || [ ! -f $BOOST_ZIP ]; then
+    echo "DOWNLOADING FAILED!"
+    exit 1
+  fi
 fi
 
-if [ ! -f $BOOST_ZIP ]; then
-  echo "DOWNLOADING FAILED!"
-  exit 1
-fi
-
-# ---------
-# UNPACKING
-# ---------
+# ---
 
 echo "UNPACKING $BOOST_ZIP"
 unzip $BOOST_ZIP
 
-if [ ! -d $BOOST_DIR ]; then
+if [ $? != 0 ] || [ ! -d $BOOST_DIR ]; then
   echo "UNPACKING FAILED!"
   exit 1
 fi
 
-mv $BOOST_DIR dist
-ln -s dist include
+# ---
 
-echo "DONE!"
+rm -rf dist
+mv $BOOST_DIR dist
+
+ln -s dist include
